8f37d00d651878a9dae32dcea8a0e9f76e7283b4
Armaan Hirani <hiraniarmaan@icloud.com>
2022-05-22 18:11:41 -0500

    inital commit -5/22/22 @ 6:11 PM

---
+++ SonOfAnton.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+public class SonOfAnton 
+{
+
+    private File codeFile;
+
+    public SonOfAnton()
+    {
+
+        codeFile = new File("code.txt");
+
+    }
+
+    public void start() 
+    {
+    
+        
+    
+    }
+    
+}
---
+++ SonOfAntonLauncher.java
@@ -0,0 +1,12 @@
+public class SonOfAntonLauncher 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        SonOfAnton soa = new SonOfAnton();
+        soa.start();
+
+    }
+
+}
